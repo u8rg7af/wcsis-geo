b77fad3df554bc7d38c33a5a05c479e0abfa06e7
Helmuth Walder <hel-electronics@gmx.at>
2018-03-25 07:48:32 +0200

    init

---
+++ map.js
@@ -0,0 +1,48 @@
+var mymap = L.map('mapid').setView([47.583807, 12.1736679], 18);
+//var map = L.map('map').setView([45.8167, 15.9833], 10);
+
+var LeafIcon = L.Icon.extend({
+    options: {
+       iconSize:     [38, 40],
+       iconAnchor:   [22, 94],
+       popupAnchor:  [-3, -76]
+    }
+});
+var openIcon = new LeafIcon({
+    iconUrl: 'cup.png',
+})
+
+var doneIcon = new LeafIcon({
+    iconUrl: 'heart.png',
+})
+
+
+L.tileLayer('https://maps4.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg' , {
+}).addTo( mymap );
+var marker = L.marker([47.583807, 12.1736679], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var marker2 = L.marker([47.583707, 12.173679], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var marker3 = L.marker([47.583707, 12.173579], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var marker4 = L.marker([47.583807, 12.173579], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+
+mymap.removeControl(mymap.zoomControl);
+
+if (!navigator.geolocation){
+    alert("Geolocation is not supported by your browser");
+  }else{
+    function success(position) {
+        console.log(position.coords.latitude);
+        L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap);
+    }
+    
+    function error() {
+        alert("Sorry, no position available.");
+    }
+    
+    var geo_options = {
+        enableHighAccuracy: false, 
+        maximumAge        : 5000, 
+        timeout           : 4000
+    };
+    
+    navigator.geolocation.watchPosition(success, error, geo_options);
+  }
